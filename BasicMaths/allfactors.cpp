41e130769de5f205b8bec7dbb1a7d59658a11bb4
Umang Hirani <umanghirani.exe@gmail.com>
2025-06-24 10:34:56 +0530

    fixed all-prime-factors + monotonic-stack started

--- BasicMaths/allfactors.cpp
+++ BasicMaths/allfactors.cpp
@@ -1,25 +1,35 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
+
 int main() {
-int n;
-cin>>n;
-unordered_map<int,int> mp;
-int answer, count = 0;
-// mp[1]++;
-while(n>1){
-    for(int i = 2;i<=n;i++){
-        count++;
-        if((n%i)==0){
-            answer = i;
-            mp[i]++;
+    int n;
+    cin >> n;
+    unordered_map<int, int> mp;
+    int answer = -1, count = 0;
+
+    while (n > 1) {
+        bool found = false;
+        for (int i = 2; i <= (int)sqrt(n) + 1; i++) {
+            count++;
+            if ((n % i) == 0) {
+                answer = i;
+                mp[i]++;
+                n = n / i;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            // n is prime
+            mp[n]++;
+            count++;
             break;
         }
     }
-    n = n/answer;
-}
-for(auto [i,freq]:mp){
-    cout<<i<<" : "<<freq<<endl;
-}
-cout<<count<<"\n";
+
+    for (auto [i, freq] : mp) {
+        cout << i << " : " << freq << endl;
+    }
+    cout << count << "\n";
 }
